05dfa3508ffc2a6abb29525925b29a3c95203acd
melerpito <melealessandro23@gmail.com>
2023-01-25 10:55:53 +0100

    Aggiunta classe Image in Models

---
+++ ImageResize/Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImageResize.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }                 //Codice univoco rappresentante l'immagine
+        public string? Name { get; set; }           //Nome assegnato all'immagine dall'utente
+        public string? ImageUrl { get; set; }       //Path dell'immagine
+        public int Width { get; set; }              //Larghezza
+        public int Height { get; set; }             //Altezza
+
+        [NotMapped]
+        public FormFile? realFigure { get; set; }   //Contiene l'immagine, non e' memorizzato nel database
+    }
+}
