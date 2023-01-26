db9ba73ee20ac1bb7cba00c86f408e7cb647d6c1
melerpito <melealessandro23@gmail.com>
2023-01-26 10:43:56 +0100

    Aggiunto un minimo di Exception Handling

--- ImageResize/Controllers/ImagesController.cs
+++ ImageResize/Controllers/ImagesController.cs
@@ -16,7 +16,7 @@ namespace ImageResize.Controllers
 
         //summary: permette l'inserimento di una nuova immagine
         //params:
-        //  imageIn: contiene l'immagine da caricare
+        //      - imageIn: contiene l'immagine da caricare
         //returns:
         //  BadRequest():
         //      - se non e' stato inserito un nome o ha piu' di 100 caratteri
@@ -34,35 +34,35 @@ namespace ImageResize.Controllers
             if(imageIn.realFigure == null)
                 return BadRequest("Necessario caricare un immagine");
 
-            //verifico se il nome e' stato gia' utilizzato
-            var imageDb = _dbContext.Figures.Where(u => u.Name == imageIn.Name).SingleOrDefault();
-            if(imageDb != null)
-                BadRequest("Nome gia' utilizzato");
-            
-            //creo un path per la memorizzazione dell'immagine in wwwroot
-            var newPath = Path.Combine("wwwroot", imageIn.Name + ".jpg");
-
             try
             {
+                //verifico se il nome e' stato gia' utilizzato
+                var imageDb = _dbContext.Figures.Where(u => u.Name == imageIn.Name).SingleOrDefault();
+                if(imageDb != null)
+                    BadRequest("Nome gia' utilizzato");
+            
+                //creo un path per la memorizzazione dell'immagine in wwwroot
+                var newPath = Path.Combine("wwwroot", imageIn.Name + ".jpg");
+
+
                 //creo un nuovo file
                 var fileStream = new FileStream(newPath, FileMode.Create);
                 //copio l'immagine nel nuovo file
                 imageIn.realFigure.CopyTo(fileStream);
-
                 //chiudo il file
                 fileStream.Close();
+
+                //memorizzo l'url dell'immagine senza wwwroot
+                imageIn.ImageUrl = newPath.Remove(0, 8);
+            
+                //memorizzo l'immagine all'interno del database
+                _dbContext.Figures.Add(imageIn);
+                _dbContext.SaveChanges();
             }
-            catch 
+            catch
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-
-            //memorizzo l'url dell'immagine senza wwwroot
-            imageIn.ImageUrl = newPath.Remove(0, 8);
-            
-            //memorizzo l'immagine all'interno del database
-            _dbContext.Figures.Add(imageIn);
-            _dbContext.SaveChanges();
 
             return StatusCode(StatusCodes.Status201Created);
         }
@@ -75,19 +75,26 @@ namespace ImageResize.Controllers
         [HttpGet]
         public IActionResult ListImages()
         {
-            //crea la lista delle immagini da restituire
-            var imagesList =
-            (
-                from images in _dbContext.Figures
-                orderby images.Name
-                select images.Name
-            );
+            try 
+            { 
+                //crea la lista delle immagini da restituire
+                var imagesList =
+                (
+                    from images in _dbContext.Figures
+                    orderby images.Name
+                    select images.Name
+                );
 
-            //controllo se esistono immagini
-            if(imagesList == null)
-                return NotFound("Nessuna immagine");
+                //controllo se esistono immagini
+                if(imagesList == null)
+                    return NotFound("Nessuna immagine");
 
-            return Ok(imagesList);
+                return Ok(imagesList);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         //summary: Elimina una immagine relativa a name
@@ -100,33 +107,34 @@ namespace ImageResize.Controllers
         [HttpDelete("{name}")]
         public IActionResult DeleteImage(string name)
         {
-            //controllo se e' stato inserito un nome
+            //controllo se e' stato inserito un nome, non dovrebbe capitare
             if(name == null)
                 return BadRequest("Necessario inserire il nome dell'immagine da eliminare");
 
             //controllo se il nome esiste
-            var imageDb = _dbContext.Figures.Where(u => u.Name == name).SingleOrDefault();
-            if (imageDb == null || imageDb.ImageUrl == null)
-                return NotFound("Immagine non trovata");
-
-            //elimino l'immagine contenuta in wwwroot
-            var imagePath = Path.Combine("wwwroot", imageDb.ImageUrl);
-            //var imagePath = "wwwroot/" + imageDb.ImageUrl;
-            if(!System.IO.File.Exists(imagePath))
-                NotFound("Immagine non trovata");
-
             try
-            { 
-                System.IO.File.Delete(imagePath); 
-            }
-            catch 
             {
+                var imageDb = _dbContext.Figures.Where(u => u.Name == name).SingleOrDefault();
+                if (imageDb == null || imageDb.ImageUrl == null)
+                    return NotFound("Immagine non trovata");
+
+                //elimino l'immagine contenuta in wwwroot
+                var imagePath = Path.Combine("wwwroot", imageDb.ImageUrl);
+                //var imagePath = "wwwroot/" + imageDb.ImageUrl;
+                if (!System.IO.File.Exists(imagePath))
+                    NotFound("Immagine non trovata");
+
+                System.IO.File.Delete(imagePath);
+
+                //Rimuovo i metadati relativi dal database
+                _dbContext.Figures.Remove(imageDb);
+                _dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
-
-            //Rimuovo i metadati relativi dal database
-            _dbContext.Figures.Remove(imageDb);
-            _dbContext.SaveChanges();
 
             return Ok("Immagine rimossa correttamente");
         }
@@ -137,40 +145,47 @@ namespace ImageResize.Controllers
         //          - width:    Rappresenta la nuova larghezza dell'immagine
         //          - height:   Rappresenta la nuova altezza dell'immagine
         //returns:
-        //      BadRequest():   se name == null o width < 1 o height < 1
-        //      NoFound():      se l'immagine non e' stata trovata
-        //      Ok():           se l'immagine e' stata modificata con successo
+        //          - BadRequest():   se name == null o width < 1 o height < 1
+        //          - NoFound():      se l'immagine non e' stata trovata
+        //          - Ok():           se l'immagine e' stata modificata con successo
         [HttpPut("{name}")]
         public IActionResult ResizeImage(string name, int width, int height)
         {
             //controllo dei parametri di ingresso
             if (name == null || width < 1 || height < 1)
                 return BadRequest("Parametri non sono inseriti corretamente");
-
-            //verifico l'esistenza dell'immagine
-            var imageDb = _dbContext.Figures.Where(u => u.Name == name).SingleOrDefault();
-            if (imageDb == null || imageDb.Name == null) 
-                return NotFound("Immagine non trovata");
-
-            var imagePath = Path.Combine("wwwroot", imageDb.Name + ".jpg");
-
-            //Ottengo il nome completo del file
-            var f = new FileInfo(imagePath);
-            var fullName = f.FullName;
-
-            // Source: https://github.com/dlemstra/Magick.NET/blob/main/docs/ResizeImage.md
-            // Read from file
-            using (var image = new MagickImage(fullName))
+            try
             {
-                var size = new MagickGeometry(100, 100);
-                // This will resize the image to a fixed size without maintaining the aspect ratio.
-                // Normally an image will be resized to fit inside the specified size.
-                size.IgnoreAspectRatio = true;
+                //verifico l'esistenza dell'immagine
+                var imageDb = _dbContext.Figures.Where(u => u.Name == name).SingleOrDefault();
+                if (imageDb == null || imageDb.Name == null) 
+                    return NotFound("Immagine non trovata");
 
-                image.Resize(size);
+                var imagePath = Path.Combine("wwwroot", imageDb.Name + ".jpg");
 
-                // Save the result
-                image.Write(fullName);
+                //Ottengo il nome completo del file
+                var f = new FileInfo(imagePath);
+                var fullName = f.FullName;
+
+                // Source: https://github.com/dlemstra/Magick.NET/blob/main/docs/ResizeImage.md
+                // Read from file
+                using (var image = new MagickImage(fullName))
+                {
+                    var size = new MagickGeometry(100, 100);
+                    // This will resize the image to a fixed size without maintaining the aspect ratio.
+                    // Normally an image will be resized to fit inside the specified size.
+                    size.IgnoreAspectRatio = true;
+
+                    image.Resize(size);
+
+                    // Save the result
+                    image.Write(fullName);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
             return Ok("Immagine modificata correttamente");
